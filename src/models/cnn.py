481f3a361a328f026afed07f3a1f5cd9483f4ea3
Ludovic Potvin <ludopotvin@gmail.com>
2026-03-07 14:51:41 -0500

    add cnn architecture

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,58 +1,34 @@
-from torch import flatten
-import torch.nn as nn
+import os
+from datetime import datetime
+
+import torch
+
+from config import RESULT_PATH
+from model_info import ModelInfo
+from cnn_architecture import CNNClassifier
 
 
-class CNNClassifier(nn.Module):
-    def __init__(self):
-        super(CNNClassifier, self).__init__()
+model = CNNClassifier()
 
-        # Conv layers
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.BN1 = nn.BatchNorm2d(32)
-        self.relu1 = nn.ReLU()
 
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.BN2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU()
+def save(model, timestamp, epoch):
+    os.makedirs(RESULT_PATH, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f"cnn_{timestamp}_epoch{epoch + 1}.pth"
+    torch.save(model.state_dict(), os.path.join(RESULT_PATH, file_name))
 
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        self.BN3 = nn.BatchNorm2d(128)
-        self.relu3 = nn.ReLU()
 
-        # Pools
-        self.pool = nn.MaxPool2d(2, 2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+def load(file_name):
+    model = CNNClassifier()
+    model.load_state_dict(torch.load(os.path.join(RESULT_PATH, file_name)))
+    return model
 
-        # FC layers
-        self.fc1 = nn.Linear(128, 64)
-        self.relufc1 = nn.ReLU()
 
-        self.fc2 = nn.Linear(64, 2)
-
-    def forward(self, x):
-        # Conv
-        z1 = self.conv1(x)
-        a1 = self.BN1(z1)
-        a1 = self.relu1(a1)
-        a1 = self.pool(a1)
-
-        z2 = self.conv2(a1)
-        a2 = self.BN2(z2)
-        a2 = self.relu2(a2)
-        a2 = self.pool(a2)
-
-        z3 = self.conv3(a2)
-        a3 = self.BN3(z3)
-        a3 = self.relu3(a3)
-        a3 = self.pool(a3)
-
-        # Transition
-        a4 = self.avgpool(a3)
-        a4 = flatten(a4, 1)
-
-        # Fc
-        z5 = self.fc1(a4)
-        a5 = self.relufc1(z5)
-        y = self.fc2(a5)
-
-        return y
+FINETUNING = ModelInfo(
+    weights=None,
+    model=model,
+    mean=[0.5, 0.5, 0.5],
+    std=[0.5, 0.5, 0.5],
+    save=save,
+    load=load,
+)
